60c86438679eed4d0b8fe2d01937d3dcfdadb424
sadhikari0102 <sadhikari0102@ufl.edu>
2018-07-24 05:42:50 -0400

    application backend initialized

---
+++ src/main/java/com/walmart/replenisher/TaskReplenisher.java
@@ -0,0 +1,12 @@
+package com.walmart.replenisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskReplenisher {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TaskReplenisher.class, args);
+	}
+}
